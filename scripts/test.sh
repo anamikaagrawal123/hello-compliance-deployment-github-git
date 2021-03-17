cb499cedaeb2194129762758e5475c914bf9bbe8
Tamas Muncsan <tamas.muncsan@ibm.com>
2021-03-17 15:21:58 +0100

    fix: exit code of acceptance test

--- scripts/test.sh
+++ scripts/test.sh
@@ -33,13 +33,19 @@ npm ci
 # iterate over inventory deployment delta
 # and run acceptance tests
 #
+exit_code=0
 for INVENTORY_ENTRY in $(echo "${DEPLOYMENT_DELTA}" | jq -r '.[] '); do
-
   APP_URL_PATH="$(echo ${INVENTORY_ENTRY} | sed 's/\//_/g')_app-url.json"
+  if [[ -f "../$APP_URL_PATH" ]]; then
+    export APP_URL=$(cat "../${APP_URL_PATH}")
 
-  export APP_URL=$(cat "../${APP_URL_PATH}")
-  npm run acceptance-test
-  ((test_count+=1))
+    echo "Running acceptance test for: '$APP_URL_PATH'"
+    if ! npm run acceptance-test; then
+      exit_code=1
+    fi
+    ((test_count+=1))
+  fi
 done
 
 echo "Run $test_count tests for $(echo "${DEPLOYMENT_DELTA}" | jq '. | length') entries"
+exit $exit_code
